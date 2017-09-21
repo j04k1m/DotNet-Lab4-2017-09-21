06d92330bdd35a7c454ea21574dba0bb6e7f91c4
Linkan <linus@ringheim.se>
2017-09-21 15:25:48 +0200

    Add project files.

---
+++ Lab_4_V1_2017_09_21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_V1_2017_09_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
